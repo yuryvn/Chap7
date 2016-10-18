115ca3b024e5c056754c78cdf2f8d073ac491be0
yuryvn <yury.v.n@gmail.com>
2016-10-18 23:24:48 +0200

    Initialization Chap7

---
+++ Chap7.cpp
@@ -0,0 +1,12 @@
+// Chap7.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "ClassesChap7.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
---
+++ ClassesChap7.cpp
@@ -0,0 +1,2 @@
+#include "stdafx.h"
+#include "ClassesChap7.h"
