f040294a35b06c76514e2a9f92b3185fd95830fd
yuryvn <yury.v.n@gmail.com>
2016-10-19 14:55:33 +0200

    update 7.1

--- ClassesChap7.cpp
+++ ClassesChap7.cpp
@@ -1,2 +1,50 @@
 #include "stdafx.h"
 #include "ClassesChap7.h"
+#include <string>
+
+using namespace std;
+
+DateT::DateT(int Day, int Month, int Year){
+	Set(Day, Month, Year);
+}
+
+DateT::~DateT(){}
+
+int DateT::CheckDay(int TestDay){
+	int DaysinMonth[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	if (M == 2){
+		if (TestDay == 29 && (Y % 400 == 0 || (Y % 4 == 0 && Y % 100 != 0))){
+			return TestDay;
+		}
+	}
+
+	if (TestDay > 0 && TestDay <= DaysinMonth[M - 1]) return TestDay;
+
+	cout << "incorrect day, setting day=1" << endl;
+	return 1;
+}
+
+void DateT::Set(int DD, int MM, int YYYY){
+	if (MM > 0 && MM <= 12) M = MM;
+	else {
+		cout << "incorrect month, setting M=1" << endl;
+		M = 1;
+	}
+
+	if (YYYY > 1969 && YYYY < 2016) Y = YYYY;
+	else {
+		cout << "incorect year, settin Y=1970";
+		Y = 1970;
+	}
+	D = CheckDay(DD);
+}
+
+void DateT::Print() const {
+	cout << D << "/" << M << "/" << Y << endl;
+}
+
+
+
+EmployeeT::EmployeeT(char *Navn, char *SurNavn, int BD, int BM, int BY, int HD, int HM, int HY){
+
+}
--- ClassesChap7.h
+++ ClassesChap7.h
@@ -1,2 +1,30 @@
 #pragma once
 
+class DateT
+{
+public:
+	DateT(int Day=1,int Month=1,int Year=1980);
+	~DateT();
+	void Set(int DD, int MM, int YYYY);
+	void Print() const;
+
+private:
+	int D, M, Y;
+	int CheckDay(int TestDay);
+
+};
+
+
+class EmployeeT
+{
+public:
+	EmployeeT(char *Navn="Navn",char *SurNavn="Navnenssen",int BD=1,int BM=1,int BY=1980,int HD=1,int HM=1,int HY=2000);
+	~EmployeeT();
+	void Set(char *Navn = "Navn", char *SurNavn = "Navnenssen", int BD = 1, int BM = 1, int BY = 1980, int HD = 1, int HM = 1, int HY = 2000);
+	void Print() const;
+
+private:
+	char *Name, *Surname;
+	DateT BirthDate, HireDate;
+
+};
